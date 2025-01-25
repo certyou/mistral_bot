f871fb3f4129020aa9bcd80205ebb616ff54a574
Ewen <biduletheboss@gmail.com>
2025-01-25 21:56:46 +0100

    add interaction with user

--- test.py
+++ test.py
@@ -6,13 +6,16 @@ model = "mistral-large-latest"
 
 client = Mistral(api_key=api_key)
 
-chat_response = client.chat.complete(
-    model= model,
-    messages = [
-        {
-            "role": "user",
-            "content": "What is the best French cheese?",
-        },
-    ]
-)
-print(chat_response.choices[0].message.content)
+def ask(msg):
+    chat_response = client.chat.complete(
+        model= model,
+        messages = [
+            {
+                "role": "user",
+                "content": msg,
+            },
+        ]
+    )
+    print(chat_response.choices[0].message.content)
+
+ask(input("une question : "))
